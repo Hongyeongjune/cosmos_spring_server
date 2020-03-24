d23dbfb82a7e4f037adca0c3d43ab01ea1c1c319
hongyeongjune <hongyeongjune@naver.com>
2020-03-24 12:12:23 +0900

    Version 1.0.1

--- src/test/java/kr/ac/skuniv/cosmos/CosmosApplicationTests.java
+++ src/test/java/kr/ac/skuniv/cosmos/CosmosApplicationTests.java
@@ -3,11 +3,11 @@ package kr.ac.skuniv.cosmos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CosmosApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
