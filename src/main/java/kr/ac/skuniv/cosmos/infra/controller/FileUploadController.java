b0a76267b185b59f7334867bb81d775a4ede7f7d
hongyeongjune <hongyeongjune@naver.com>
2020-07-14 16:12:53 +0900

    Version 1.0.0

---
+++ src/main/java/kr/ac/skuniv/cosmos/infra/controller/FileUploadController.java
@@ -0,0 +1,26 @@
+package kr.ac.skuniv.cosmos.infra.controller;
+
+import kr.ac.skuniv.cosmos.infra.S3Uploader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/cosmos/kStars")
+@RequiredArgsConstructor
+public class FileUploadController {
+
+    private final S3Uploader s3Uploader;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upload(@RequestParam("musicFile")MultipartFile multipartFile){
+        String fileName = UUID.randomUUID().toString();
+        return ResponseEntity.ok(s3Uploader.uploadFile(multipartFile, fileName));
+    }
+
+}
