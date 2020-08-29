31f16414ebdac3626dd2b951de5ae9509e79f87d
hongyeongjune <hongyeongjune@naver.com>
2020-08-29 20:32:02 +0900

    Member CRUD 작성

---
+++ src/main/java/kr/ac/skuniv/cosmos/domain/dto/member/MemberGetDto.java
@@ -0,0 +1,31 @@
+package kr.ac.skuniv.cosmos.domain.dto.member;
+
+import kr.ac.skuniv.cosmos.domain.entity.member.Member;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberGetDto {
+
+    private Long id;
+    private String email;
+
+    @Builder
+    public MemberGetDto(Long id,
+                        String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+
+
+    public static MemberGetDto toDto(Member member){
+        return MemberGetDto.builder()
+                .email(member.getEmail())
+                .id(member.getId())
+                .build();
+    }
+}
